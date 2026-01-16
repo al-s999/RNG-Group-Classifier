31cbac9cf0aa1c18e827932ed342c2468cf066fb
al-s999 <ahmadrosyidalfualdi@gmail.com>
2026-01-16 17:39:19 +0700

    Add OpenNext config for Cloudflare

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+// open-next.config.ts
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+
+export default defineCloudflareConfig({
+  // config minimal, biarkan default dulu
+});
